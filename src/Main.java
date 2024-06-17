6f5431bd80a81a3159853227692e39f9385f6f77
krollchristensen <krollchristensen@gmail.com>
2024-06-17 08:34:53 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // Deklaration og initialisering af variabler
+        byte myByte = 127;
+        short myShort = 32000;
+        int myInt = 2147483647;
+        long myLong = 9223372036854775807L;
+        float myFloat = 3.14f;
+        double myDouble = 3.141592653589793;
+        boolean myBoolean = true;
+        char myChar = 'A';
+
+        // Udskrivning af variablernes værdier
+        System.out.println("Byte value: " + myByte);
+        System.out.println("Short value: " + myShort);
+        System.out.println("Int value: " + myInt);
+        System.out.println("Long value: " + myLong);
+        System.out.println("Float value: " + myFloat);
+        System.out.println("Double value: " + myDouble);
+        System.out.println("Boolean value: " + myBoolean);
+        System.out.println("Char value: " + myChar);
+    }
+}
